ee343d88b37f44b0a01a14cfa92b743f89fba98d
Vadimotry <vadim.krsk09@gmail.com>
2024-12-18 00:33:43 +0700

    Update CheckingAccount.h

--- CheckingAccount.h
+++ CheckingAccount.h
@@ -1,16 +1,36 @@
-#ifndef CHECKINGACCOUNT_H
-#define CHECKINGACCOUNT_H
+#include "CheckingAccount.h"
+#include <iostream>
+#include <iomanip>
 
-#include "Account.h"
+CheckingAccount::CheckingAccount(double initialBalance, double overdraftLimit)
+    : Account(initialBalance), overdraftLimit(overdraftLimit) {
+}
 
-class CheckingAccount : public Account {
-private:
-    double overdraftLimit;
-public:
-    CheckingAccount(double initialBalance, double overdraftLimit = 0);
-    void Withdraw(double amount) override;
-    void DisplayInfo() const override;
-    void SetOverdraftLimit(double newLimit);
-};
+void CheckingAccount::Withdraw(double amount) {
+    if (balance + overdraftLimit >= amount) {
+        balance -= amount;
+        if (balance < 0) {
+            std::cout << "Вы вышли в минус на " << std::fixed << std::setprecision(2) << -balance << "!" << std::endl;
+        }
+        else {
+            std::cout << "Снятие: " << std::fixed << std::setprecision(2) << amount << " выполнено." << std::endl;
+        }
+    }
+    else {
+        std::cout << "Недостаточно средств для снятия!" << std::endl;
+    }
+}
 
-#endif
+void CheckingAccount::DisplayInfo() const {
+    std::cout << "Расчетный счет, баланс: " << std::fixed << std::setprecision(2) << balance
+        << ", Лимит овердрафта: " << std::fixed << std::setprecision(2) << overdraftLimit << std::endl;
+}
+
+void CheckingAccount::SetOverdraftLimit(double newLimit) {
+    overdraftLimit = newLimit;
+    std::cout << "Лимит овердрафта обновлен до: " << std::fixed << std::setprecision(2) << newLimit << std::endl;
+}
+
+double CheckingAccount::GetOverdraftLimit() const {
+    return overdraftLimit;
+}
